93318350d185600c4ad38e46b6f732f6f0a84a7a
Kristina Lubinets <kristinalubinets@Kristinas-MBP.lan>
2021-12-02 03:45:23 -0500

    add cart #

--- head.php
+++ head.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <title>Home</title>
 <link rel="stylesheet" type="text/css" href="home_style.css"/>
+<link rel="stylesheet" type="text/css" href="navbar.css"/>
 
 <!-- Compressed CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.4/dist/css/foundation.min.css"
--- navbar.php
+++ navbar.php
@@ -20,8 +20,8 @@
 
     <div class="navbar-end">
         <a class="cart-link" href="/arena350/cart.php">
-            <!--            <span>1</span>-->
-            <svg value=1 version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            <span>1</span>
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                  x="0px" y="0px"
                  viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
 <g>
